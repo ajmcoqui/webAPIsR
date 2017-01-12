6dea1efa48a75cc5334a200a15e1545e29d688fb
Amanda Gadrow <amanda.gadrow@rstudio.com>
2017-01-12 15:18:54 -0500

    Streamline swapi script and add presentation Rmd

--- swapi.R
+++ swapi.R
@@ -2,22 +2,30 @@ library(httr)
 library(jsonlite)
 library(magrittr)
 
-# Note the pattern of verb, URL, and parameters
+# Goal: Get the data for the planet Alderaan
+# verb (method) = GET
+# URL (endpoint) = http://swapi.co/api/planets/
+# parameter = search
+
 alderaan <- GET("http://swapi.co/api/planets/?search=alderaan")
+# Same call in a different format
 # alderaan <- GET("http://swapi.co/api/planets/", query = list(search = "alderaan"))
+
 stop_for_status(alderaan)
 names(alderaan)
 alderaan$status_code
 alderaan$headers$`content-type`
 
 # Get the content of the response
-text_content <- content(alderaan, as = "text")
+text_content <- content(alderaan, as = "text", encoding = "UTF-8")
 text_content
 
 # Parse with httr
 parsed_content <- content(alderaan, as = "parsed")
 parsed_content
 names(parsed_content)
+parsed_content$count
+parsed_content$`next`
 str(parsed_content$results)
 parsed_content$results[[1]]$name
 parsed_content$results[[1]]$terrain
@@ -34,7 +42,7 @@ planetary_data$terrain
 
 # Helper function
 json_parse <- function(req) {
-  text <- content(req, as = "text")
+  text <- content(req, as = "text", encoding = "UTF-8")
   if (identical(text, "")) warn("No output to parse.")
   fromJSON(text)
 }
@@ -48,6 +56,7 @@ json_planets <- json_parse(planets)
 # The response includes metadata as well as results
 names(json_planets)
 json_planets$count
+length(json_planets$results$name)
 json_planets$`next`
 
 swapi_planets <- json_planets$results
@@ -64,20 +73,25 @@ parsed_next_page$results$name
 # If the API results come back paged like this, you can write a loop to follow the next URL 
 # until the there are no more pages, and rbind all the data into a single dataframe.
 
-# Grab data on all of the Star Wars planets in 11 lines of code:
-planets <- GET("http://swapi.co/api/planets") %>% stop_for_status()
-json_planets <- planets %>% json_parse
-swapi_planets <- json_planets$results
-next_page <- json_planets$`next`
+# Grab data on all of the Star Wars planets
+planets <- GET("http://swapi.co/api/planets") %>% 
+  stop_for_status() %>% 
+  json_parse
+swapi_planets <- planets$results
+
+next_page <- planets$`next`
 while(!is.null(next_page)) {
-  more_planets <- GET(next_page) %>% stop_for_status() %>% json_parse
-  next_page <- more_planets$`next`
+  more_planets <- GET(next_page) %>% 
+    stop_for_status() %>% 
+    json_parse
   swapi_planets <- rbind(swapi_planets, more_planets$results)
+  next_page <- more_planets$`next`
 }
+
 length(swapi_planets$name)
 swapi_planets$name
 
-# In real life, you'd also want to handle any errors, headers, and rate limits as needed.
+# In real life, you'd also want to handle any errors, headers, rate limits, etc. as needed.
 help(package=httr)
 
 # Someone wrote a package for swapi:  https://github.com/Ironholds/rwars
